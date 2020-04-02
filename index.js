63c1ed520b8cff5fc02978b08aab146146da68b6
lakshyavijh <lakshya.vijh99@gmail.com>
2020-04-02 18:16:58 +0530

    broadcasting messages

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 var express = require('express');
-var socket= require('socket.io');
+var socket = require('socket.io');
 
-//App setup
+// App setup
 var app = express();
-
-var server = app.listen(4000,function() {
-    console.log("Listening to requests on 4000");
+var server = app.listen(4000, function(){
+    console.log('listening for requests on port 4000,');
 });
 
-//static files
+// Static files
 app.use(express.static('public'));
 
-//Socekt setup
+// Socket setup & pass server
 var io = socket(server);
+io.on('connection', (socket) => {
 
-io.on('connection',function(socket){
+    console.log('made socket connection', socket.id);
 
-     // Handle chat event
-     socket.on('chat', function(data){
+    // Handle chat event
+    socket.on('chat', function(data){
         // console.log(data);
         io.sockets.emit('chat', data);
     });
 
-    console.log("made socket connection" ,socket.id);
+    // Handle typing event
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
+
 });
